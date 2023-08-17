e12fa3f779b88640361075c132027d39e1a78023
DimaDBRK <dbrkdima@gmail.com>
2023-08-17 12:26:42 +0300

    server js update for deploy

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ import u_router from "./routes/users.js";
 import wbapi_router from "./routes/wbapi.js";
 import cookieParser from "cookie-parser";
 
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 
 // add parser
@@ -26,3 +32,13 @@ app.listen(process.env.PORT, () => {
     console.log(`run on port ${process.env.PORT}`);
     });
     
+
+// for deploy
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
