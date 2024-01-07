48c50ee8dac9edc06cd213ce01d42b48ed59eec8
DimaDBRK <dbrkdima@gmail.com>
2024-01-07 14:43:11 +0200

    base api update

--- helpers/GetLiveDataTest.js
+++ helpers/GetLiveDataTest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const GetLiveData = async () => {
   console.log("Get live data start");
     try {
-      const res = await axios.get(`https://data.binance.com/api/v3/ticker/24hr`);
+      const res = await axios.get(`https://api.binance.com/api/v3/ticker/24hr`);
       if (res.status===200 ) {
         // console.log(res.data[0].quoteVolume)
         const point = {"x":res.data[0].closeTime, "y": res.data[0].quoteVolume}
